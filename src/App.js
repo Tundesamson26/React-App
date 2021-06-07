b57b0e4d2ed0d08334105c8902996c768fa540ab
Hitesh <hdk.hitesh26@gmail.com>
2021-06-07 20:45:25 +0530

    issue fixed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import classes from './App.module.css';
+import './App.css';
 import ProductData from './ProductData';
 
 
@@ -11,45 +11,45 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <nav className={classes.Topbar}>
+        <nav className="Topbar">
           <img src="https://www.logo.wine/a/logo/Amazon_(company)/Amazon_(company)-Logo.wine.svg" alt="Amazon" />
         </nav>
       </header>
       
-      <div className ={classes.MainContainer}>
-        <div className ={classes.ProductPreview}>
+      <div className ="MainContainer">
+        <div className ="ProductPreview">
           <img src="https://imgur.com/iOeUBV7.png" alt="Product Preview"/>
 
           {/* <div className={classes.TimeSection}>
             <p>{`${currentHour}:${currentMinute}`}</p>
           </div> */}
 
-          <div className={classes.HeartBeatSection}>
-          <i class="fas fa-heartbeat"></i>
+          <div className="HeartBeatSection">
+          <i className="fas fa-heartbeat"></i>
             <p>78</p>
           </div>
 
         </div>
         
-        <div className ={classes.ProductData}>
-          <h1 className={classes.ProductTitle}>{ProductData.title}</h1>
-          <p className={classes.ProductDescription}>{ProductData.description}</p>
+        <div className ="ProductData">
+          <h1 className="ProductTitle">{ProductData.title}</h1>
+          <p className="ProductDescription">{ProductData.description}</p>
 
-          <h3 className={classes.SectionHeadings}>Select colour</h3>
+          <h3 className={"SectionHeadings"}>Select colour</h3>
           <div>
-            <img className={[classes.ProductImage, classes.SelectedProductImage].join(' ')} src="https://imgur.com/iOeUBV7.png"alt="Black Coloured Watch" />
-            <img className={classes.ProductImage} src="https://imgur.com/PTgQlim.png"alt="Red Coloured Watch" />
-            <img className={classes.ProductImage} src="https://imgur.com/Mplj1YR.png"alt="Blue Coloured Watch" />
-            <img className={classes.ProductImage} src="https://imgur.com/xSIK4M8.png"alt="Purple Coloured Watch" />
+            <img className="ProductImage SelectedProductImage" src="https://imgur.com/iOeUBV7.png"alt="Black Coloured Watch" />
+            <img className="ProductImage" src="https://imgur.com/PTgQlim.png"alt="Red Coloured Watch" />
+            <img className="ProductImage" src="https://imgur.com/Mplj1YR.png"alt="Blue Coloured Watch" />
+            <img className="ProductImage" src="https://imgur.com/xSIK4M8.png"alt="Purple Coloured Watch" />
           </div>
 
-          <h3 className={classes.SectionHeadings}>Features</h3>
+          <h3 className="SectionHeadings">Features</h3>
           <div>
-            <button className={[classes.FeaturesItem, classes.SelectedFeaturesItem].join(' ')}>Rate</button>
-            <button className={classes.FeaturesItem}>Heart Rate</button>
+            <button className="FeaturesItem SelectedFeaturesItem">Rate</button>
+            <button className="FeaturesItem">Heart Rate</button>
           </div>
 
-          <button className={classes.PrimaryButton}>Buy Now</button>
+          <button className="PrimaryButton">Buy Now</button>
         </div>
       </div>
     </div>
